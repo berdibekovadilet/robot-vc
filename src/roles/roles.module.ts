0bfb9e86212b2033b77554addd229e64e82cbaa0
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-20 19:43:59 +0600

    added UserRole model

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { User } from "src/users/users.model";
 import { RolesController } from "./roles.controller";
 import { Role } from "./roles.model";
 import { RolesService } from "./roles.service";
+import { UserRole } from "./user-roles.model";
 
 @Module({
   controllers: [RolesController],
   providers: [RolesService],
-  imports: [SequelizeModule.forFeature([Role])],
+  imports: [SequelizeModule.forFeature([Role, User, UserRole])],
 })
 export class RolesModule {}
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from "sequelize-typescript";
+import { Role } from "src/roles/roles.model";
+import { UserRole } from "src/roles/user-roles.model";
 
 interface UserCreationAttr {
   email: string;
@@ -32,4 +40,7 @@ export class User extends Model<User, UserCreationAttr> {
   @ApiProperty({ example: "За спам", description: "Причина бана" })
   @Column({ type: DataType.STRING })
   banReason: string;
+
+  @BelongsToMany(() => Role, () => UserRole)
+  users: Role[];
 }
